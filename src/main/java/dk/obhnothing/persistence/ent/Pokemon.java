0da91773cdb39918f367cfaba3c099b399bb82f6
oskar123456 <cph-oh82@cphbusiness.dk>
2024-10-27 12:46:31 +0100

    o

--- src/main/java/dk/obhnothing/persistence/ent/Pokemon.java
+++ src/main/java/dk/obhnothing/persistence/ent/Pokemon.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @ToString
---
+++ src/main/java/dk/obhnothing/persistence/service/Mapping.java
@@ -0,0 +1,15 @@
+package dk.obhnothing.persistence.service;
+
+import dk.obhnothing.persistence.ent.Pokemon;
+
+public class Mapping
+{
+
+    public static Pokemon PokemonDTO_Pokemon(PokemonDTO p_dto)
+    {
+
+        Pokemon pMon = new Pokemon();
+
+    }
+
+}
